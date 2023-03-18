0ffd76475c74475422dad440dd3a4b6f93330866
Andrew Booker <improvizone@gmail.com>
2023-03-18 17:13:56 +0000

    working out what sources are available during the sequence timeline

--- video/parseEdits.py
+++ video/parseEdits.py
@@ -33,6 +33,40 @@ def anyOtherFrom(others, pos, s, currentAlias):
             dur = s - pos
             return (o, start, dur)
 
+class SourceSeqs():
+    def __init__(self):
+        self.start = {}
+        self.end = {}
+        for alias in lengths:
+            spl = alias.split("_")
+            syncKey = spl[0]
+
+            if syncKey in sync:
+                if len(spl) > 1 and int(spl[1]) > 1:
+                    prevKey = "%s_%d" % (syncKey, int(spl[1]) - 1)
+                    self.start[alias] = self.end[prevKey]
+                else:
+                    self.start[alias] = sync[syncKey]
+                self.end[alias] = self.start[alias] + lengths[alias]
+                print(alias, self.start[alias], self.end[alias])
+
+    def availableLengthAt(self, t, alias, currentAlias):
+        if alias in self.start and t >= self.start[alias] and t < self.end[alias]:
+            available = self.end[alias] - t
+            if available > 0:
+                return available
+        return 0
+
+sourceSeqs = SourceSeqs()
+
+def listOptionsFrom(others, pos, currentAlias):
+    r = []
+    for o in others:
+        t = pos - sync[currentAlias]
+        available = sourceSeqs.availableLengthAt(t, o, currentAlias)
+        if available and o not in currentAlias  :
+            r.append((o, available))
+    return r
 
 pos = 0
 for e in edits:
@@ -50,8 +84,20 @@ for e in edits:
             d = k["end"] - s
 
             if pos != s:
+                print("between split screen at", pos - sync[this], ":", listOptionsFrom(others, pos, this))
                 print("[\"%s\", [%d, %d]]" % anyOtherFrom(others, pos, s, this))
                 pos = s
-            print("[\"%s\", [%d, %d], { \"splitScreenWith\": [\"apeman\", \"dragon_2\", \"apexman_1\"] }]," % (this, s, d))
-            pos += d
+
+            splitOptions = listOptionsFrom(others, pos, this)
+            availableDur = d
+            for o in splitOptions:
+                if o[1] < availableDur:
+                    availableDur = o[1]
+            ssw = { "splitScreenWith": [o[0] for o in splitOptions] }
+
+            print("[\"%s\", [%d, %0.2f], %s]," % (this, pos, availableDur, json.dumps(ssw)))
+            pos += availableDur
+            if availableDur != d:
+                print("[\"%s\", [%0.2f, %0.2f], %s]," % (this, pos, d - availableDur, json.dumps({ "splitScreenWith": [o[0] for o in listOptionsFrom(others, pos, this)] })))
+            pos += (d - availableDur)
 
