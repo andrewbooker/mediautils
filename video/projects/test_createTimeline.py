8c2a993cd26394a3882058968a466d6d3791264b
Andrew Booker <improvizone@gmail.com>
2023-03-19 12:18:35 +0000

    recognising single edit to accompany a source

--- video/projects/test_createTimeline.py
+++ video/projects/test_createTimeline.py
@@ -3,19 +3,32 @@
 class Timeline():
     def __init__(self, srcs, edits = {}):
         self.srcs = srcs
-    
+        self.edits = edits
+
     def create(self):
         t = []
         for s in self.srcs:
             src = self.srcs[s]
-            t.append([src["name"], [0, src["length"]]])
+            start = 0
+            dur = src["length"]
+            if s in self.edits:
+                edit = self.edits[s]["edits"][0]
+                if "start" in edit:
+                    start = edit["start"]
+                    dur = src["length"] - start
+                if "end" in edit:
+                    dur = edit["end"] - start
+
+            t.append([src["name"], [start, dur]])
         return t
-        
+
+
+
 def test_returns_nothing_given_no_data():
     t = Timeline({})
     assert t.create() == []
 
-def test_lists_single_available_source():
+def test_lists_single_available_source_with_no_edits():
     t = Timeline({
         "thing.mp4": {
             "name": "closeUp",
@@ -23,3 +36,49 @@ def test_lists_single_available_source():
         }
     })
     assert t.create() == [["closeUp", [0, 211.98]]]
+
+def test_breaks_up_single_source_given_single_edit_specifying_start():
+    srcs = {
+        "thing.mp4": {
+            "name": "closeUp",
+            "length": 211.98
+        }
+    }
+    edits = {
+        "thing.mp4": {
+            "edits": [{ "start": 5 }]
+        }
+    }
+    t = Timeline(srcs, edits)
+    assert t.create() == [["closeUp", [5, 206.98]]]
+
+def test_breaks_up_single_source_given_single_edit_specifying_end():
+    srcs = {
+        "thing.mp4": {
+            "name": "closeUp",
+            "length": 211.98
+        }
+    }
+    edits = {
+        "thing.mp4": {
+            "edits": [{ "end": 11 }]
+        }
+    }
+    t = Timeline(srcs, edits)
+    assert t.create() == [["closeUp", [0, 11]]]
+
+def test_breaks_up_single_source_given_single_edit_specifying_start_and_end():
+    srcs = {
+        "thing.mp4": {
+            "name": "closeUp",
+            "length": 211.98
+        }
+    }
+    edits = {
+        "thing.mp4": {
+            "edits": [{ "start": 5, "end": 11 }]
+        }
+    }
+    t = Timeline(srcs, edits)
+    assert t.create() == [["closeUp", [5, 6]]]
+
