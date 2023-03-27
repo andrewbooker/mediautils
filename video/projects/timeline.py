4295cd7b29a47833b3bf017a6436b556ca631191
Andrew Booker <improvizone@gmail.com>
2023-03-27 21:16:20 +0100

    simplification to reflect only one item is represented as playing

--- video/projects/timeline.py
+++ video/projects/timeline.py
@@ -12,7 +12,7 @@ class Timeline():
     def project(events, syncs):
         events.sort(key=lambda e: e["ts"])
         playable = []
-        playing = {}
+        playing = None
         tl = []
         splitMode = SplitMode.NotSplit
 
@@ -28,36 +28,31 @@ class Timeline():
                 if splitMode == SplitMode.Started and len(playable) < 4:
                     splitMode = SplitMode.Stopped
 
-            if len(events) > (i + 1) and events[i + 1]["ts"] != e["ts"] and len(playable) > 0 and len(playing.items()) == 0:
+            if len(events) > (i + 1) and events[i + 1]["ts"] != e["ts"] and len(playable) > 0 and playing is None:
                 if len(playable) == 4:
                     splitMode = SplitMode.Started
-                playing[playable[0]] = e["ts"]
+                playing = (playable[0], e["ts"])
 
-            if len(playing.items()) > 0:
-                toRemove = []
-                toAdd = []
-                for (k, v) in playing.items():
-                    sync = syncs[k] if k in syncs else 0
-                    if k not in playable or splitMode == SplitMode.ShouldStart:
-                        d = e["ts"] - v
-                        if d > 0 and splitMode != SplitMode.Stopped:
-                            tl.append([k, [v + sync, d]])
-                            if splitMode == SplitMode.ShouldStart:
-                                splitMode = SplitMode.Started
-                        toRemove.append(k)
-                        if len(playable) > 0:
-                            toAdd.append(playable[0])
-                    if splitMode == SplitMode.ShouldStart:
-                        splitMode = SplitMode.Started
-                    if splitMode == SplitMode.Stopped:
-                        otherPlayable = filter(lambda p: k not in p, playable)
-                        tl.append([k, [v + sync, e["ts"] - v], { "splitScreenWith": [p for p in otherPlayable] }])
-                        splitMode = SplitMode.NotSplit
-                for k in toRemove:
-                    del playing[k]
-                if len(toAdd) > 0:
-                    playing[toAdd[0]] = e["ts"] #subject to weighting
+            if playing is not None:
+                (k, v) = playing
+                sync = syncs[k] if k in syncs else 0
+                if k not in playable or splitMode == SplitMode.ShouldStart:
+                    d = e["ts"] - v
+                    if d > 0 and splitMode != SplitMode.Stopped:
+                        tl.append([k, [v + sync, d]])
+                        if splitMode == SplitMode.ShouldStart:
+                            splitMode = SplitMode.Started
+                    playing = None
+                    if len(playable) > 0:
+                        playing = (playable[0], e["ts"]) #subject to weighting
+                if splitMode == SplitMode.ShouldStart:
+                    splitMode = SplitMode.Started
+                if splitMode == SplitMode.Stopped:
+                    otherPlayable = filter(lambda p: k not in p, playable)
+                    tl.append([k, [v + sync, e["ts"] - v], { "splitScreenWith": [p for p in otherPlayable] }])
+                    splitMode = SplitMode.NotSplit
 
+            print(e["ts"], playable)
         return tl
 
     @staticmethod
