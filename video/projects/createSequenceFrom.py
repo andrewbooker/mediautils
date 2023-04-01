0fea92e1670b209b81fafe2bbdc888cc93448677
Andrew Booker <improvizone@gmail.com>
2023-04-01 17:01:57 +0100

    running createSequenceFrom ffmpeg commands instead of dumping them to the console

--- video/projects/createSequenceFrom.py
+++ video/projects/createSequenceFrom.py
@@ -7,7 +7,7 @@ import json
 import cv2
 
 inDir = sys.argv[1]
-files = os.listdir(inDir)
+files = filter(lambda f: not os.path.isdir(os.path.join(inDir, f)) and ".txt" not in f, os.listdir(inDir))
 title = sys.argv[2]
 number = int(sys.argv[3])
 
@@ -18,7 +18,7 @@ if not os.path.exists(d):
 
 patterns = [
     (r"202[3-9]_[0-9]{4}_.*\.MOV", "apeman"),
-    (r"FILE.*\.MOV", "apexman"),
+    (r"FILE.*\.MOV", "apexcam"),
     (r"NORM.*\.MP4", "dragon"),
     (r"[0-9]{8}_[0-9]{6}\.mp4", "G5")
 ]
@@ -42,7 +42,7 @@ for f in files:
             if p[1] not in aliasCount:
                 aliasCount[p[1]] = 0
             aliasCount[p[1]] += 1
-            if p[1] in ["apexman", "dragon"]:
+            if p[1] in ["apexcam", "dragon"]:
                 if p[1] not in toJoin:
                     toJoin[p[1]] = []
                 toJoin[p[1]].append({ "file": f, "length": length })
@@ -57,12 +57,10 @@ for (a, fs) in toJoin.items():
             tl += f["length"]
             concat.write("file '%s'\n" % f["file"])
             outfile = "%s.%s" % (a, f["file"].split(".")[1])
-    print("ffmpeg -safe 0 -f concat -i %s -c copy -y %s" % (cf, os.path.join(inDir, outfile)))
+    os.system("ffmpeg -safe 0 -f concat -i %s -c copy -y %s" % (cf, os.path.join(inDir, outfile)))
     out["aliases"][outfile] = { "name": a, "length": tl }
 
 
-if False:
-    with open(os.path.join(d, "sequence.json"), "w") as js:
-        json.dump(out, js, indent=4)
-else:
-    print(json.dumps(out, indent=4))
+with open(os.path.join(d, "sequence.json"), "w") as js:
+    json.dump(out, js, indent=4)
+
