f5185e05f7089f16e82f369d9e2d2e7630e1cf75
Andrew Booker <improvizone@gmail.com>
2023-04-05 09:14:40 +0100

    slowly partitioning hardcoded text into utility functions

--- video/projects/textTest.py
+++ video/projects/textTest.py
@@ -3,8 +3,7 @@
 import os
 import sys
 
-applyToFn = sys.argv[1]
-outDir = sys.argv[2]
+
 charsPerSec = 20.0
 textBaseY = 800
 titleSize = 130
@@ -41,45 +40,50 @@ def writeScrollText(what, vf, startAt, readingTime, y):
         vf.append(":".join(txt))
     return readingTime + (len(what) * 1.0 / charsPerSec)
 
-opening = "Randomatones"
-toScroll = [
-    "single-board computer ambient music generators",
-    "recorded at outdoor locations with natural reverb",
-    "this is episode 19",
-]
-title = "Regents Canal Park Road"
 
+def applyTextTo(vf, number, title, location):
+    toScroll = [
+        "single-board computer ambient music generators",
+        "recorded at outdoor locations with natural reverb",
+        "this is episode %d" % number,
+    ]
+
+    readingTime = 4
+    startTime = 4
+    for i in range(len(toScroll)):
+        s = toScroll[i]
+        print(len(s), "chars", len(s) * 1.0 / charsPerSec, startTime)
+        startTime += writeScrollText(s, vf, startTime, readingTime, textBaseY + titleSize)
+
+    writeText("Randomatones", vf, 2, startTime - 2, titleSize, textBaseY)
+
+    coordsDur = writeScrollText(location, vf, startTime + readingTime, reqTime - startTime, textBaseY + titleSize)
+    writeText(title, vf, startTime, coordsDur + readingTime, 100, textBaseY)
+
+
+    endingTextStart = totalRunningTime - 14
+    madeByDur = writeScrollText("made by", vf, endingTextStart, 10, textBaseY)
+    nameOffSet = 1
+    writeText("Andrew Booker", vf, endingTextStart + nameOffSet, madeByDur - nameOffSet, 80, textBaseY + 50)
+
+def applyFadeTo(vf):
+    vf.append("fade=type=in:duration=3,fade=type=out:duration=4:start_time=%d" % (totalRunningTime - 4))
+
+
+vf = []
+applyTextTo(vf, 19, "Regents Canal Park Road", "latitude 51.5284447 longitude -0.1669526 03 Mar 2023 6.30pm")
+applyFadeTo(vf)
+
+applyToFn = sys.argv[1]
+outDir = sys.argv[2]
 
 cmd = []
 cmd.append("ffmpeg")
 cmd.append("-i")
 cmd.append(applyToFn)
-
-vf = []
-
-readingTime = 4
-startTime = 4
-for i in range(len(toScroll)):
-    s = toScroll[i]
-    print(len(s), "chars", len(s) * 1.0 / charsPerSec, startTime)
-    startTime += writeScrollText(s, vf, startTime, readingTime, textBaseY + titleSize)
-
-writeText(opening, vf, 2, startTime - 2, titleSize, textBaseY)
-
-coordsDur = writeScrollText("latitude 51.5284447 longitude -0.1669526 03 Mar 2023 6.30pm", vf, startTime + readingTime, reqTime - startTime, textBaseY + titleSize)
-writeText(title, vf, startTime, coordsDur + readingTime, 100, textBaseY)
-
-
-endingTextStart = totalRunningTime - 14
-madeByDur = writeScrollText("made by", vf, endingTextStart, 10, textBaseY)
-nameOffSet = 1
-writeText("Andrew Booker", vf, endingTextStart + nameOffSet, madeByDur - nameOffSet, 80, textBaseY + 50)
-vf.append("fade=type=in:duration=3,fade=type=out:duration=4:start_time=%d" % (totalRunningTime - 4)) 
-
 if len(vf):
     cmd.append("-vf")
     cmd.append("\"%s\"" % ",".join(vf))
-
 cmd.append("-y")
 cmd.append(os.path.join(outDir, "with_text.mp4"))
 
