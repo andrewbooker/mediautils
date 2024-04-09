111b484e98a4afd24965ae50c9e4692af9aab088
Andrew Booker <improvizone@gmail.com>
2024-04-09 13:45:31 +0100

    cleaning up and updating spash page script for Randomatones

--- video/splashPageText.py
+++ video/splashPageText.py
@@ -3,23 +3,22 @@
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import json
 import os
 import sys
 
-def rgb(a):
-	return "rgb%s" % str(tuple(a))
 
 workingDir = sys.argv[1]
+storyboardDir = os.path.join(workingDir, "storyboard")
 frameFn = sys.argv[2]
 title = sys.argv[3]
-headingSize = 170
-titleSize = headingSize + 10
+headingSize = 130
+titleSize = int(headingSize * 0.8)
 headingStart = 50
 
-img = Image.open(os.path.join(workingDir, frameFn))
+img = Image.open(os.path.join(storyboardDir, frameFn))
 d = ImageDraw.Draw(img)
-d.text([60, headingStart], "Randomatones", fill="yellow", font=ImageFont.truetype("impact.ttf", headingSize))
+d.text([60, headingStart], "Randomatones.", fill="yellow", font=ImageFont.truetype("impact.ttf", headingSize))
+d.text([60 + (len("Randomatones") * 0.535 * headingSize), int(headingStart * 1.54)], "26", fill="yellow", font=ImageFont.truetype("impact.ttf", int(headingSize * 0.8)))
 d.text([60, headingStart + headingSize], title, fill="orange", font=ImageFont.truetype("impact.ttf", titleSize))
 
 img.save(os.path.join(workingDir, "splash_%s.jpg" % frameFn.split(".")[0]))
