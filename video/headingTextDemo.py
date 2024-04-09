4dba103cabd854e21dac47eba83ad7a05051b2f3
Andrew Booker <improvizone@gmail.com>
2024-04-09 13:44:33 +0100

    accepting colour word in heading text demo

--- video/headingTextDemo.py
+++ video/headingTextDemo.py
@@ -5,18 +5,20 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import os
 import sys
+import re
 
 
 storyboardDir = os.path.join(sys.argv[1], "storyboard")
-colourHex = sys.argv[2]
+c = sys.argv[2]
+colour = f"#{c}" if re.search("^[0-9|A-E]{6}$", c.upper()) is not None else c
 sceneIdx = int(sys.argv[3]) if len(sys.argv) > 3 else 0
 files = [f for f in sorted(os.listdir(storyboardDir), key=lambda fn: int(fn.split("_")[0]))]
 img = Image.open(os.path.join(storyboardDir, files[sceneIdx]))
 d = ImageDraw.Draw(img)
 headingSize = 120
 headingYPos = 800
-d.text([60, headingYPos], "Randomatones", fill=f"#{colourHex}", font=ImageFont.truetype("impact.ttf", headingSize))
+d.text([60, headingYPos], "Randomatones", fill=colour, font=ImageFont.truetype("impact.ttf", headingSize))
 
-img.save(os.path.join(sys.argv[1], f"scene{sceneIdx}_{colourHex}.jpg"))
+img.save(os.path.join(sys.argv[1], f"scene{sceneIdx}_{colour}.jpg"))
 img.close()
 
