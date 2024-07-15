85817d6f45b887c086e8266d66008bcef0efb903
Andrew Booker <improvizone@gmail.com>
2024-07-15 21:23:28 +0100

    spalsh page text adjustments tailored for cwmbran

--- video/splashPageText.py
+++ video/splashPageText.py
@@ -8,19 +8,26 @@ import sys
 
 
 workingDir = sys.argv[1]
-storyboardDir = os.path.join(workingDir, "storyboard")
 frameFn = sys.argv[2]
 title = sys.argv[3]
-headingSize = 130
+headingSize = 90
 titleSize = int(headingSize * 0.8)
-headingStart = 50
+headingStart = 730
 
-img = Image.open(os.path.join(storyboardDir, frameFn))
+
+titleColour = "#5190ff"
+episodeColour = "#ff2d00"
+
+img = Image.open(os.path.join(workingDir, frameFn))
 d = ImageDraw.Draw(img)
-d.text([60, headingStart], "Randomatones.", fill="yellow", font=ImageFont.truetype("impact.ttf", headingSize))
-d.text([60 + (len("Randomatones") * 0.535 * headingSize), int(headingStart * 1.54)], "26", fill="yellow", font=ImageFont.truetype("impact.ttf", int(headingSize * 0.8)))
-d.text([60, headingStart + headingSize], title, fill="orange", font=ImageFont.truetype("impact.ttf", titleSize))
+d.text([60, headingStart], "Randomatones.", fill=titleColour, font=ImageFont.truetype("impact.ttf", headingSize))
 
-img.save(os.path.join(workingDir, "splash_%s.jpg" % frameFn.split(".")[0]))
+d.text([60 + (len("Randomatones") * 0.532 * headingSize), int(headingStart + (headingSize * 0.2))], "30", fill=titleColour, font=ImageFont.truetype("impact.ttf", int(headingSize * 0.8)))
+
+d.text([60, headingStart + headingSize], title, fill=episodeColour, font=ImageFont.truetype("impact.ttf", titleSize))
+
+outFn = os.path.join(workingDir, "splash_%s.png" % frameFn.split(".")[0])
+print("saving as", outFn)
+img.save(outFn)
 img.close()
 
