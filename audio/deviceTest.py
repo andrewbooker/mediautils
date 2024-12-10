3b1de411212a29e5d87e27e30821ff94bfc4038f
Andrew Booker <improvizone@gmail.com>
2024-12-10 21:02:28 +0000

    checking in prepping to play sound using pygame (not yet working)

--- audio/deviceTest.py
+++ audio/deviceTest.py
@@ -14,6 +14,7 @@ channels = int(sys.argv[2]) if len(sys.argv) > 1 else 1
 if device is None:
     exit()
 
+use_pg = True
 sound = []
 sampleRate = 44100
 durSecs = 2
@@ -32,10 +33,41 @@ for i in range(sampleRate * durSecs):
         sample.append(v * level)
     sound.append(sample)
 
-sd.default.channels = channels
-sd.default.device = device
-
-sd.play(sound, sampleRate)
-time.sleep(len(sound) / (1.0 * sampleRate))
-
 print("using device", device)
+
+if not use_pg:
+    sd.default.channels = channels
+    sd.default.device = device
+
+    sd.play(sound, sampleRate)
+    time.sleep(len(sound) / (1.0 * sampleRate))
+else:
+    import pygame as pg
+    import numpy as np
+    
+    print("initialising pygame")
+    pg.init()
+    print("initialising mixer")
+    pg.mixer.init(frequency=sampleRate, size=-16, channels=channels, buffer=1024, devicename="HDA NVidia: HDMI 0 (hw:1,7), ALSA (0 in, 8 out)")
+    pg.mixer.set_num_channels(3) # simultaneous notes
+    
+    print("converting buffer to byte array")
+    cs = []
+    for c in range(channels):
+        cs.append([s[c] for s in sound])
+    print(len(cs))
+    print(len(sound))
+    ba = np.array(cs)
+    print("creating pg sound")
+    pgs = pg.mixer.Sound(array=ba)
+    
+    print("selecting a channel")
+    channel = pg.mixer.find_channel()
+    channel.set_volume(level)
+    print("playing")
+    channel.play(pgs)
+    print("waiting to stop")    
+    time.sleep(3)
+    pg.quit()
+            
+
