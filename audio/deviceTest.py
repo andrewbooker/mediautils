e48b8799ccca9ecd5d0f311fc1e785cbddef9be3
Andrew Booker <improvizone@gmail.com>
2024-12-10 21:41:45 +0000

    getting basic pygame version of audio device test working

--- audio/deviceTest.py
+++ audio/deviceTest.py
@@ -16,10 +16,10 @@ level = float(sys.argv[3]) if len(sys.argv) > 3 else 1.0
 if device is None:
     exit()
 
-use_pg = False
+use_pg = True
 sound = []
 sampleRate = 44100
-durSecs = 3
+durSecs = 3 
 fHz = 220
 
 
@@ -34,16 +34,17 @@ rampDownStart = totalDur - (sampleRate + rampTime)
 
 for i in range(int(totalDur)):
     sample = []
+    vol = level
+    if i > sampleRate and i < rampUpFinish:
+        vol *= ((i - sampleRate) / rampTime)
+    elif i > rampDownStart and i < (totalDur - sampleRate):
+        vol *= (1.0 - ((i - rampDownStart) / rampTime))
+
     for c in range(channels):
         if i < sampleRate or i > (sampleRate * (durSecs + 1)):
             sample.append(0.0)
         else:
             v = 1.0 + (0.5 * (math.sin(i * fHz * 2 * math.pi * (1.0 + detune[c]) / (1.0 * sampleRate))))
-            vol = level
-            if i > sampleRate and i < rampUpFinish:
-                vol *= ((i - sampleRate) / rampTime)
-            elif i > rampDownStart and i < (totalDur - sampleRate):
-                vol *= (1.0 - ((i - rampDownStart) / rampTime))
             sample.append(v * vol)
     sound.append(sample)
 
@@ -59,31 +60,33 @@ if not use_pg:
     print("stopping")
 else:
     import pygame as pg
-    import numpy as np
+    import numpy
     
     print("initialising pygame")
     pg.init()
     print("initialising mixer")
-    pg.mixer.init(frequency=sampleRate, size=-16, channels=channels, buffer=1024, devicename="HDA NVidia: HDMI 0 (hw:1,7), ALSA (0 in, 8 out)")
-    pg.mixer.set_num_channels(3) # simultaneous notes
+    pg.mixer.init(frequency=sampleRate, size=-16, channels=channels, buffer=1024, devicename="HDA NVidia: EP-HDMI-RX (hw:1,7), ALSA (0 in, 8 out)")
+    pg.mixer.set_num_channels(1) # simultaneous notes
     
-    print("converting buffer to byte array")
-    cs = []
-    for c in range(channels):
-        cs.append([s[c] for s in sound])
-    print(len(cs))
     print(len(sound))
-    ba = np.array(cs)
-    print("creating pg sound")
-    pgs = pg.mixer.Sound(array=ba)
+    print("converting buffer to byte array")
+    buf = numpy.zeros((len(sound), channels), dtype = numpy.int16)
+    max_sample = 2**(16 - 1) - 1
+    for s in range(len(sound)):
+        for c in range(channels):
+            buf[s][c] = int(max_sample * sound[s][c])
     
+    print(len(buf))
+    print(len(buf[0]))
+    print("creating pg sound")
+    pgs = pg.mixer.Sound(array=buf)    
     print("selecting a channel")
     channel = pg.mixer.find_channel()
     channel.set_volume(level)
     print("playing")
     channel.play(pgs)
     print("waiting to stop")    
-    time.sleep(3)
+    time.sleep(durSecs + 3)
     pg.quit()
             
 
