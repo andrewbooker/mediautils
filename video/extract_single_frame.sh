b08561146d3a435beb50d0c351a538b3e8bfda42
Andrew Booker <improvizone@gmail.com>
2026-06-21 11:50:16 +0100

    demoing title text at the specified point

--- video/extract_single_frame.sh
+++ video/extract_single_frame.sh
@@ -1,1 +1,1 @@
-ffmpeg -ss $2 -i $1 -vf "select=eq(n\,0)" -vframes 1 -y $3
+ffmpeg -ss $2 -i $1 -vf "select=eq(n\,0),scale=1920x1080" -vframes 1 -y $3
--- video/textColourDemo.py
+++ video/textColourDemo.py
@@ -3,6 +3,9 @@
 import sys
 import os
 import json
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 def toSecs(s):
     if type(s) != str:
@@ -15,34 +18,6 @@ def load():
     with open(seqFn, "r") as seqF:
         return json.load(seqF)
 
-rawDir = sys.argv[1]
-seqFn = sys.argv[2]
-baseOutDir = sys.argv[3]
-
-res_options = [
-    "1920x1080",
-    "768x432",
-    "3840x2160"
-]
-
-res = int(sys.argv[4]) if len(sys.argv) > 4 else 0
-loRes = res == 1
-resolution = res_options[res]
-horiz = int(resolution.split("x")[0])
-vert = int(resolution.split("x")[1])
-
-textColourTestsDir = os.path.join(baseOutDir, "textColourTests")
-if not os.path.exists(textColourTestsDir):
-    os.makedirs(textColourTestsDir)
-
-
-j = load()
-text = j["text"]
-
-srcs = []
-
-headingStart = text["heading"]["start"]
-print(headingStart)
 
 class Sequence:
     def __init__(self):
@@ -71,13 +46,54 @@ class Sequence:
                 fqfn_in = os.path.join(rawDir, fn)
                 fqfn_out = os.path.join(textColourTestsDir, f"{fnb}_{pos}.jpg")
                 os.system(f"./extract_single_frame.sh {fqfn_in} {pos} {fqfn_out} 2>/dev/null")
-                return (rt, fqfn_out, start, pos)
+                return (fqfn_out, start, pos, rt)
+
+rawDir = sys.argv[1]
+seqFn = sys.argv[2]
+baseOutDir = sys.argv[3]
+
+res_options = [
+    "1920x1080",
+    "768x432",
+    "3840x2160"
+]
+
+res = int(sys.argv[4]) if len(sys.argv) > 4 else 0
+loRes = res == 1
+resolution = res_options[res]
+horiz = int(resolution.split("x")[0])
+vert = int(resolution.split("x")[1])
+
+textColourTestsDir = os.path.join(baseOutDir, "textColourTests")
+if not os.path.exists(textColourTestsDir):
+    os.makedirs(textColourTestsDir)
+
+j = load()
+if not "text" in j:
+    print("sequence has no text")
+    exit
+
+spec = j["text"]
+srcs = []
+
 
 seq = Sequence()
 seq.load_from(j)
-print(seq.filesByStartTime)
 
-print(seq.file_at(4))
-print(seq.file_at(6))
-print(seq.file_at(14))
+headingStart = spec["heading"]["start"]
+masterColour = spec["colour"] if "colour" in spec else "white"
+madeByColour = spec["madeBy"]["colour"] if "madeBy" in spec and "colour" in spec["madeBy"] else masterColour
+headingYPos = spec["yPos"] if "yPos" in spec else 60
+
+print(headingStart)
+fhs = seq.file_at(headingStart)
+
+img = Image.open(fhs[0]).convert("RGB")
+d = ImageDraw.Draw(img)
+titleSize = 130
+scrollTextSize = 50
+
+d.text([60, headingYPos], "Randomatones", fill=masterColour, font=ImageFont.truetype("impact.ttf", titleSize))
+img.save(fhs[0])
+img.close()
 
